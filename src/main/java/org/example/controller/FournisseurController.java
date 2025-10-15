01836c5386cfb6b2e38782b37b6efd6782425868
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-10-15 10:00:00 +0100

    Readme

--- src/main/java/org/example/controller/FournisseurController.java
+++ src/main/java/org/example/controller/FournisseurController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/fournisseurs")
+@RequestMapping("/api/v1/fournisseurs")
 public class FournisseurController {
     private FournisseurService fournisseurService;
 
